527d08229a3d3a8cb1795a82e1d3b63c0f067aed
stephta <stephanieta@hotmail.ca>
2025-03-01 15:56:39 -0800

    fix capitalization

--- src/footer.py
+++ src/footer.py
@@ -3,7 +3,7 @@ from dash import html
 def footer():
     return html.Footer(
         children=[
-            html.P("cookie-dash interactive Dashboard empowers home bakers and culinary enthusiasts to filter and compare chocolate chip cookie recipes based on ingredient types, ratings, and complexity using dynamic visualizations.",
+            html.P("Cookie Dash interactive dashboard empowers home bakers and culinary enthusiasts to filter and compare chocolate chip cookie recipes based on ingredient types, ratings, and complexity using dynamic visualizations.",
                     style={"font-size": "12px"}),
             html.P("Developers: Mu (Henry) Ha, Javier Martinez, Stephanie Ta, and Zuer (Rebecca) Zhong.",
                        style={"font-size": "12px"}),
