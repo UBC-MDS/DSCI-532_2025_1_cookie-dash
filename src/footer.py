7146a51778f802c70a621fec0e37c448828423d0
stephta <stephanieta@hotmail.ca>
2025-03-01 15:55:51 -0800

    fix a bit of footer formatting

--- src/footer.py
+++ src/footer.py
@@ -9,12 +9,12 @@ def footer():
                        style={"font-size": "12px"}),
             html.A("GitHub Repository", href="https://github.com/UBC-MDS/DSCI-532_2025_1_cookie-dash",
                        target="_blank", style={"font-size": "12px"}),
-            html.P("Last updated on March 1, 2025",
+            html.P("Last updated: March 1, 2025",
                        style={"font-size": "12px"}),
             ],
         className="p-2",
         style={
-            "backgroundColor": "#f08080",
+            "backgroundColor": "#fff",
             "height": "5vh", 
             "margin": "1vh 5vw",
         }
