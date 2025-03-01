5282cac98ad45892abbfd097c4a2461bd9bd9de7
stephta <stephanieta@hotmail.ca>
2025-03-01 15:53:41 -0800

    add info to footer

--- src/footer.py
+++ src/footer.py
@@ -2,7 +2,16 @@ from dash import html
 
 def footer():
     return html.Footer(
-        "Footer",
+        children=[
+            html.P("cookie-dash interactive Dashboard empowers home bakers and culinary enthusiasts to filter and compare chocolate chip cookie recipes based on ingredient types, ratings, and complexity using dynamic visualizations.",
+                    style={"font-size": "12px"}),
+            html.P("Developers: Mu (Henry) Ha, Javier Martinez, Stephanie Ta, and Zuer (Rebecca) Zhong.",
+                       style={"font-size": "12px"}),
+            html.A("GitHub Repository", href="https://github.com/UBC-MDS/DSCI-532_2025_1_cookie-dash",
+                       target="_blank", style={"font-size": "12px"}),
+            html.P("Last updated on March 1, 2025",
+                       style={"font-size": "12px"}),
+            ],
         className="p-2",
         style={
             "backgroundColor": "#f08080",
