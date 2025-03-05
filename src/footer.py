27c8666398bef1c3e9e6f6af16a2ef37410a27a3
stephta <stephanieta@hotmail.ca>
2025-03-05 11:11:52 -0800

    try to make footer text a bit tighter, had to make height as auto so that it wouldn't cut off

--- src/footer.py
+++ src/footer.py
@@ -4,18 +4,15 @@ def footer():
     return html.Footer(
         children=[
             html.P("Cookie Dash interactive dashboard empowers home bakers and culinary enthusiasts to filter and compare chocolate chip cookie recipes based on ingredient types, ratings, and complexity using dynamic visualizations.",
-                    style={"font-size": "12px"}),
-            html.P("Developers: Mu (Henry) Ha, Javier Martinez, Stephanie Ta, and Zuer (Rebecca) Zhong.",
-                       style={"font-size": "12px"}),
+                    style={"font-size": "12px", "margin-bottom": "2px"}),
+            html.P("Developers: Mu (Henry) Ha, Javier Martinez, Stephanie Ta, and Zuer (Rebecca) Zhong. Last updated: March 1, 2025",
+                       style={"font-size": "12px", "margin-bottom": "2px"}),
             html.A("GitHub Repository", href="https://github.com/UBC-MDS/DSCI-532_2025_1_cookie-dash",
-                       target="_blank", style={"font-size": "12px"}),
-            html.P("Last updated: March 1, 2025",
-                       style={"font-size": "12px"}),
+                       target="_blank", style={"font-size": "12px", "lineHeight": "1"}),
             ],
         className="p-2",
         style={
-            "backgroundColor": "#fff",
-            "height": "5vh", 
+            "height": "auto", 
             "margin": "1vh 5vw",
         }
     )
